22fd86e2567a7f49b400a81d154f03d98c1ad49a
Abhijeet Kankani <48484326+AbhijeetKankani@users.noreply.github.com>
2023-08-17 10:51:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,20 +6,32 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/run_program', methods=['POST'])
 def run_program():
-    data = request.get_json()
-    query = data['query']
+    try:
+        data = request.get_json()
+        query = data['query']
 
-    # Execute your Python program
-    output = subprocess.check_output([sys.executable, 'Chat.py', query])
+        # Print the received query for debugging
+        print("Received query:", query)
 
-    return jsonify({'output': output.decode()})
+        # Execute your Python program
+        output = subprocess.check_output([sys.executable, 'Chat.py', query])
+
+        return jsonify({'output': output.decode()})
+    except Exception as e:
+        print("Error:", e)  # Print the exception for debugging
+        # Return an error response with a 500 status code
+        return jsonify({'error': str(e)}), 500
+
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 8080))  # Use port 8080 as default
-    app.run(host='0.0.0.0', port=port)    # Bind to all available network interfaces
+    # Bind to all available network interfaces
+    app.run(host='0.0.0.0', port=port)
